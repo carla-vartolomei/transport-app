c2548da369ac498161d61f46669f55c4ee7cfb44
Carla Andreea Vartolomei <vartolomei_carla@yahoo.com>
2022-03-30 14:42:23 +0300

    .

--- components/header/Navbar.js
+++ components/header/Navbar.js
@@ -4,9 +4,6 @@ import DirectionsBusIcon from "@mui/icons-material/DirectionsBus"
 import Link from "next/link"
 import React from "react"
 import styles from "./Navbar.module.scss"
-import Menu from "@mui/material/Menu"
-import MenuItem from "@mui/material/MenuItem"
-import PopupState, { bindTrigger, bindMenu } from "material-ui-popup-state"
 
 const pages = ["home", "about", "routes", "tickets", "discounts", "contact"]
 
@@ -31,46 +28,13 @@ export default function Navbar() {
             {pages.map((item, index) => {
               let path = ""
               item === "home" ? (path = "/") : (path = `/${item}`)
-              if (item !== "routes") {
-                return (
-                  <Button key={index.toString()} color="inherit">
-                    <Link href={path}>
-                      <a>{item}</a>
-                    </Link>
-                  </Button>
-                )
-              } else {
-                return (
-                  <PopupState variant="popover">
-                    {(popupState) => (
-                      <>
-                        <Button color="inherit" {...bindTrigger(popupState)}>
-                          <Link href={path}>
-                            <a>{item}</a>
-                          </Link>
-                        </Button>
-                        <Menu {...bindMenu(popupState)}>
-                          <MenuItem onClick={popupState.close}>
-                            <Link href={`${path}/iasi-suceava`}>
-                              <a>Iasi-Suceava</a>
-                            </Link>
-                          </MenuItem>
-                          <MenuItem onClick={popupState.close}>
-                            <Link href={`${path}/iasi-falticeni`}>
-                              <a>Iasi-Falticeni</a>
-                            </Link>
-                          </MenuItem>
-                          <MenuItem onClick={popupState.close}>
-                            <Link href={`${path}/iasi-husi`}>
-                              <a>Iasi-Husi</a>
-                            </Link>
-                          </MenuItem>
-                        </Menu>
-                      </>
-                    )}
-                  </PopupState>
-                )
-              }
+              return (
+                <Button key={index.toString()} color="inherit">
+                  <Link href={path}>
+                    <a>{item}</a>
+                  </Link>
+                </Button>
+              )
             })}
           </Box>
         </Toolbar>
