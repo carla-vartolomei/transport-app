b0e9563d64a0f41ef79b8832d9abcb63c38d5bb7
Carla Andreea Vartolomei <vartolomei_carla@yahoo.com>
2022-04-01 11:24:06 +0300

    edit files

--- components/homepage/discountSection/DiscountSection.js
+++ components/homepage/discountSection/DiscountSection.js
@@ -8,32 +8,17 @@ import discountImage from "../../../assets/discounts-image.png"
 
 function DiscountSection() {
   return (
-    <Grid container spacing={2}>
-      <Grid item xs={2}>
-        <Paper className={styles.discountsImagePaper}>
-          <Container className={styles.discountsImageContainer}>
-            <Image
-              src={discountImage}
-              alt="discounts-image"
-              className={styles.discountsImage}
-            />
-          </Container>
-        </Paper>
-      </Grid>
-      <Grid item xs={10}>
-        <Box className={styles.boxAccordions}>
-          <div className={styles.discountsRoot}>
-            {data.map((item, index) => (
-              <Accordion
-                key={index.toString()}
-                accordionTitle={item.title}
-                accordionDetails={item.details}
-              />
-            ))}
-          </div>
-        </Box>
-      </Grid>
-    </Grid>
+    <Box className={styles.boxAccordions}>
+      <div className={styles.discountsRoot}>
+        {data.map((item, index) => (
+          <Accordion
+            key={index.toString()}
+            accordionTitle={item.title}
+            accordionDetails={item.details}
+          />
+        ))}
+      </div>
+    </Box>
   )
 }
 
