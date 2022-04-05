e4b9a1a34e8bbd4edbcb968ff3ee893ea378de35
Carla Andreea Vartolomei <vartolomei_carla@yahoo.com>
2022-04-05 17:14:25 +0300

    .

--- pages/about/index.js
+++ pages/about/index.js
@@ -1,3 +1,4 @@
+import { Paper } from "@material-ui/core"
 import React from "react"
 import Banner from "../../components/banner/Banner"
 import styles from "../../styles/About.module.scss"
@@ -7,6 +8,19 @@ export default function About() {
     <>
       <Banner />
       <h1 className="pageTitle">About us</h1>
+      <p className={styles.pageDescription}>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Tenetur, ipsa
+        laboriosam, doloremque perferendis ullam iure ad sunt itaque debitis eveniet
+        quidem, facilis beatae in eos consequatur nobis consectetur ipsum cum! Lorem
+        ipsum dolor sit amet consectetur adipisicing elit. Corporis aperiam ad atque
+        eligendi placeat incidunt voluptates, dolorem obcaecati consequuntur
+        voluptate quaerat quia, earum quasi neque. Commodi sint porro temporibus
+        recusandae.
+      </p>
+      <Paper className={styles.visionMissionSection} elevation={3}>
+        <h1>heelo</h1>
+        <h1>heelo</h1>
+      </Paper>
     </>
   )
 }
