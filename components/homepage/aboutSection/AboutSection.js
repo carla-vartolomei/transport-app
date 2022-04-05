ba7c00a98211f9588c755a1dd991ebc63eb8ed7b
Carla Andreea Vartolomei <vartolomei_carla@yahoo.com>
2022-04-05 19:00:02 +0300

    edit about page

--- components/homepage/aboutSection/AboutSection.js
+++ components/homepage/aboutSection/AboutSection.js
@@ -23,16 +23,5 @@ export default function AboutLayout() {
   )
 }
 
-const aboutDescription = `Lorem ipsum dolor sit amet consectetur adipisicing elit. Debitis nobis
-deleniti ad, autem ut vitae nihil praesentium rem hic quod voluptatum
-voluptas natus veritatis beatae, temporibus similique. Impedit, excepturi
-ipsam! Lorem ipsum, dolor sit amet consectetur adipisicing elit. Laudantium
-quia voluptas accusamus non sunt necessitatibus facere, expedita, ipsum fugit
-provident praesentium porro inventore cupiditate nihil, rerum quisquam
-assumenda aliquam. Lorem ipsum, dolor sit amet consectetur adipisicing elit.
-Tempore officia dolores earum repellendus id qui nobis? Atque similique ullam
-molestiae possimus debitis illum voluptatem corporis. Omnis qui
-necessitatibus provident dolores. Lorem ipsum, dolor sit amet consectetur
-adipisicing elit. Rerum odit quidem dolores eaque porro animi, hic laboriosam
-accusamus et, incidunt voluptatem ab consectetur obcaecati, officia enim
-voluptatum perspiciatis quasi? Error?`
+const aboutDescription =
+  "INTERTRANSCOM S.R.L. was founded on 03.10.2000 and since then it provides high quality transport services. The principles made this company stand out in the short time on the market as the main player in passenger transport services, and the quality of services exceeded the expectations of the beneficiaries."
--- pages/about/index.js
+++ pages/about/index.js
@@ -1,7 +1,17 @@
-import { Paper } from "@material-ui/core"
 import React from "react"
+import { Paper } from "@material-ui/core"
+import Image from "next/image"
+import vision from "../../assets/vision.png"
+import mission from "../../assets/mission.png"
+import bus from "../../assets/bus.jpg"
 import Banner from "../../components/banner/Banner"
 import styles from "../../styles/About.module.scss"
+import BadgeOutlinedIcon from "@mui/icons-material/BadgeOutlined"
+import PushPinOutlinedIcon from "@mui/icons-material/PushPinOutlined"
+import WorkOutlineOutlinedIcon from "@mui/icons-material/WorkOutlineOutlined"
+import QrCodeScannerOutlinedIcon from "@mui/icons-material/QrCodeScannerOutlined"
+import IntegrationInstructionsOutlinedIcon from "@mui/icons-material/IntegrationInstructionsOutlined"
+import AppRegistrationOutlinedIcon from "@mui/icons-material/AppRegistrationOutlined"
 
 export default function About() {
   return (
@@ -9,20 +19,67 @@ export default function About() {
       <Banner />
       <h1 className="pageTitle">About us</h1>
       <p className={styles.pageDescription}>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Tenetur, ipsa
-        laboriosam, doloremque perferendis ullam iure ad sunt itaque debitis eveniet
-        quidem, facilis beatae in eos consequatur nobis consectetur ipsum cum! Lorem
-        ipsum dolor sit amet consectetur adipisicing elit. Corporis aperiam ad atque
-        eligendi placeat incidunt voluptates, dolorem obcaecati consequuntur
-        voluptate quaerat quia, earum quasi neque. Commodi sint porro temporibus
-        recusandae.
+        INTERTRANSCOM S.R.L. was founded on 03.10.2000 and since then it provides
+        high quality transport services. The principles made this company stand out
+        in the short time on the market as the main player in passenger transport
+        services, and the quality of services exceeded the expectations of the
+        beneficiaries.
       </p>
       <Paper className={styles.visionMissionSection} elevation={3}>
         <div className={styles.visionSection}>
           <h1>vision</h1>
+          <Image src={vision} alt="company vision" width="100%" height="100%" />
+          <p>
+            Promoting the comfort, safety and reliability of passengers transport
+            actually meant only one thing: <strong>PROMOTING VALUE</strong>.
+          </p>
         </div>
         <div className={styles.missionSection}>
           <h1>mission</h1>
+          <Image src={mission} alt="company vision" width="100%" height="100%" />
+          <p>
+            To be the main player in passenger transport services and the quality of
+            services to exceed the expectations of the beneficiaries.
+          </p>
+        </div>
+      </Paper>
+      <p className={styles.pageDescription}>
+        The young management team pioneered the introduction of satellite fleet
+        monitoring, TV / DVD equipment, wireless internet of the car park, receiving
+        the appreciation of large-scale customers when occupying the place.
+      </p>
+      <Paper className={styles.companySection} elevation={3}>
+        <div className={styles.companyDataSection}>
+          <h1>company data</h1>
+          <ul className={styles.companyDataList}>
+            <li>
+              <BadgeOutlinedIcon className={styles.listItemIcon} />
+              <span>INTERTRANSCOM S.R.L.</span>
+            </li>
+            <li>
+              <PushPinOutlinedIcon className={styles.listItemIcon} />
+              <span>str. Arcu, nr. 1, bl T3, Mezanin, office 7, IASI</span>
+            </li>
+            <li>
+              <WorkOutlineOutlinedIcon className={styles.listItemIcon} />
+              <span>Other ground passenger transport</span>
+            </li>
+            <li>
+              <QrCodeScannerOutlinedIcon className={styles.listItemIcon} />
+              <span> CAEN - 4939</span>
+            </li>
+            <li>
+              <IntegrationInstructionsOutlinedIcon className={styles.listItemIcon} />
+              <span>CIF - RO13410037</span>
+            </li>
+            <li>
+              <AppRegistrationOutlinedIcon className={styles.listItemIcon} />
+              <span>REG - J22/727/20.09.2000</span>
+            </li>
+          </ul>
+        </div>
+        <div className={styles.imageSection}>
+          <Image src={bus} alt="company bus" quality={100} />
         </div>
       </Paper>
     </>
