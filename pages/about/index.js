e52af247359ee46a845d2becf9a1b1dbd0a47339
Carla Andreea Vartolomei <vartolomei_carla@yahoo.com>
2022-04-06 11:11:30 +0300

    add bold text to about page description

--- pages/about/index.js
+++ pages/about/index.js
@@ -9,11 +9,11 @@ export default function About() {
       <Banner />
       <h1 className="pageTitle">About us</h1>
       <p className="pageDescription">
-        INTERTRANSCOM S.R.L. was founded on 03.10.2000 and since then it provides
-        high quality transport services. The principles made this company stand out
-        in the short time on the market as the main player in passenger transport
-        services, and the quality of services exceeded the expectations of the
-        beneficiaries.
+        <strong>INTERTRANSCOM S.R.L.</strong> was founded on 03.10.2000 and since
+        then it provides high quality transport services. The principles made this
+        company stand out in the short time on the market as the main player in
+        passenger transport services, and the quality of services exceeded the
+        expectations of the beneficiaries.
       </p>
       <VisionMissionSection />
       <p className="pageDescription">
