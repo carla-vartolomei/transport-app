3c98216a49fe9b7484bf22d8ca1b450cb4fadfa2
Carla Andreea Vartolomei <vartolomei_carla@yahoo.com>
2022-04-20 11:54:59 +0300

    create tickets page

--- pages/tickets/index.js
+++ pages/tickets/index.js
@@ -1,5 +1,10 @@
+import { Button, Container } from "@material-ui/core"
 import React from "react"
 import Banner from "../../components/banner/Banner"
+import styles from "../../styles/Tickets.module.scss"
+import CheckIcon from "@mui/icons-material/Check"
+import CloseIcon from "@mui/icons-material/Close"
+import CssTextField from "../../components/textfield/CustomeTextField"
 
 export default function index() {
   return (
@@ -7,14 +12,33 @@ export default function index() {
       <Banner />
       <h1 className="pageTitle">Tickets</h1>
       <p className="pageDescription">
-        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Porro tempora,
-        dolor quae veniam quasi non odio eum dignissimos provident nemo. Nam
-        similique adipisci eius, dignissimos doloremque pariatur sed tenetur
-        voluptates! Lorem, ipsum dolor sit amet consectetur adipisicing elit. At
-        consequuntur accusamus ea labore asperiores facere excepturi tempora nihil
-        impedit vero ullam dolor, commodi nulla nesciunt cupiditate. Cupiditate
-        veniam blanditiis quia.
+        Tickets are proof of payment and reservation of a seat on a certain route on
+        a certain date and time. To check the existence of the ticket look for the
+        ticket by series. Also, if you want to cancel a ticket, look for it by series
+        and cancel it.
       </p>
+      <div className={styles.pageSubtitle}>
+        <CheckIcon fontSize="large" />
+        <h2>Check your ticket</h2>
+      </div>
+      <section className={styles.checkSection}>
+        <CssTextField
+          className={styles.checkField}
+          label="Enter your ticket number"
+        />
+        <Button className={styles.checkButton}>check</Button>
+      </section>
+      <div className={styles.pageSubtitle}>
+        <CloseIcon fontSize="large" />
+        <h2>Cancel your ticket</h2>
+      </div>
+      <section className={styles.cancelSection}>
+        <CssTextField
+          className={styles.cancelField}
+          label="Enter your ticket number"
+        />
+        <Button className={styles.cancelButton}>cancel</Button>
+      </section>
     </>
   )
 }
