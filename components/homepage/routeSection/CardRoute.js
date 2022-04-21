fa4b33299e2951a4265d1846fc521ff366a3d3a4
Carla Andreea Vartolomei <vartolomei_carla@yahoo.com>
2022-04-21 17:03:43 +0300

    make homepage responsive

--- components/homepage/routeSection/CardRoute.js
+++ components/homepage/routeSection/CardRoute.js
@@ -29,7 +29,7 @@ export function CardRoute({
             .split(" ")
             .join("")}`}
         >
-          <a>more info</a>
+          more info
         </Link>
       </Button>
       {contactEndPoint ? (
